a90cb02c33a0810c8df80ed6e5fe3ea2fc050279
qitianshi <tianshi.qi@icloud.com>
2020-07-10 14:36:34 +0800

    Initial Commit

---
+++ C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++
+//
+//  Created by Tian Shi Qi on 10/7/20.
+//  Copyright © 2020 Qi Tianshi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
