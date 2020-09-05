6e71a6d56c8b096a17e176563f0d5cc624df929d
qitianshi <tianshi.qi@icloud.com>
2020-09-05 22:45:53 +0800

    Completed Fibonacci
     - Adds completed files for Fibonacci.

--- Dynamic programming/Fibonacci/Fibonacci.cpp
+++ Dynamic programming/Fibonacci/Fibonacci.cpp
@@ -7,12 +7,39 @@
 // Finds the nth Fibonacci number.
 
 
-#include <iostream>
+//#include <iostream>
+//#include <utility>
+//#include <chrono>       // steady_clock
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
     
-    cout << "Hello, world!\n";
+    int target;
+    cin >> target;
+    
+    auto start = chrono::steady_clock::now();       // Starts a timer.
+    
+    unsigned long long result = (target <= 0 ? 0 : 1);      // Edge case for the zeroth or negative Fibonacci number.
+    {
+        
+        pair<unsigned long long, unsigned long long> previousNumbers = {1, 1};      // The previous number, and the one before it.
+        for (int i = 2; i < target; ++ i) {     // The loop is skipped for the first two Fibonacci numbers; the result remains at 1.
+            
+            result = previousNumbers.first + previousNumbers.second;
+            
+            // Refreshes the stored previous numbers with new values.
+            swap(previousNumbers.first, previousNumbers.second);
+            previousNumbers.first = result;
+            
+        }
+        
+    }
+    
+    auto end = chrono::steady_clock::now();         // Ends the timer.
+    
+    cout << result << '\n';
+    cout << "Completed in " << chrono::duration_cast<chrono::microseconds>(end - start).count() << " µs.\n";
     
     return 0;
     
