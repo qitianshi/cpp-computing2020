7d1559f01b707420542aeeba6292af86c61d9cf5
qitianshi <tianshi.qi@icloud.com>
2020-09-19 03:44:57 +0800

    Corrections to LongestIncreasingSubsequence and MaximumSubsequence
     - Corrects 3332591a77b27e0e13109630f6a7fca9d4307d69 where the files for LongestIncreasingSubsequence and MaximumSubsequence were swapped inadvertently.

--- Dynamic programming/MaximumSubsequence/MaximumSubsequence.cpp
+++ Dynamic programming/MaximumSubsequence/MaximumSubsequence.cpp
@@ -1,43 +1,35 @@
-// LongestIncreasingSubsequence.cpp
-// Created on 16/9/20
+// MaximumSubsequence.cpp
+// Created on 15/9/20
 // Running on macOS 10.15
 
 // GNU General Public License version 3
 
-// Finds the longest increasing subsequence in a sequence of numbers.
+// Finds the contiguous subsequence with the largest sum.
 
 
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <algorithm>
+#include <limits.h>     // INT_MIN
 using namespace std;
 
 int main() {
     
     ifstream fin("data.txt");
-    
-    // Stores the sequence in a vector.
-    vector<int> sequence;
+    vector<int> runningTotal;
+
+    // Stores the running total of the sequence.
     {
         int input;
-        while (fin >> input) { sequence.push_back(input); }
+        while (fin >> input) { runningTotal.push_back((runningTotal.empty() ? input : runningTotal[runningTotal.size() - 1] + input)); }
     }
     
-    vector<int> longestIncreasing;      // The length of the LIS that ends at each position.
-    longestIncreasing.push_back(1);     // Pushes 1 for the zeroth element because there cannot be longer subsequences.
-    
-    for (int i = 1; i < sequence.size(); ++ i) {
-        
-        longestIncreasing.push_back(1);     // Initial size
-        
-        for (int j = 0; j < i; ++ j) {      // Loops through previous positions in the sequence.
-            if ( (sequence[i] > sequence[j]) && (longestIncreasing[i] < longestIncreasing[j] + 1) ) { longestIncreasing[i] = longestIncreasing[j] + 1; }
-        }
-        
+    int maximum = INT_MIN;
+    for (int i = 1; i <= runningTotal.size() - 1; ++ i) for (int j = 0; j < runningTotal.size() - i; ++ j) {
+        if (runningTotal[j + i] - runningTotal[j] > maximum) { maximum = runningTotal[j + i] - runningTotal[j]; }
     }
     
-    cout << *max_element(longestIncreasing.begin(), longestIncreasing.end()) << '\n';
+    cout << maximum << '\n';
     
     return 0;
     
