3dfd7ce48e07c15a67682e506ccc4914f49be78d
qitianshi <tianshi.qi@icloud.com>
2020-09-19 03:26:06 +0800

    Completes MaximumSubsequence

--- Dynamic programming/MaximumSubsequence/MaximumSubsequence.cpp
+++ Dynamic programming/MaximumSubsequence/MaximumSubsequence.cpp
@@ -8,12 +8,29 @@
 
 
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <limits.h>     // INT_MIN
 using namespace std;
 
 int main() {
     
-    cout << "Hello, world!\n";
-     
+    ifstream fin("data.txt");
+    vector<int> runningTotal;
+
+    // Stores the running total of the sequence.
+    {
+        int input;
+        while (fin >> input) { runningTotal.push_back((runningTotal.empty() ? input : runningTotal[runningTotal.size() - 1] + input)); }
+    }
+    
+    int maximum = INT_MIN;
+    for (int i = 1; i <= runningTotal.size() - 1; ++ i) for (int j = 0; j < runningTotal.size() - i; ++ j) {
+        if (runningTotal[j + i] - runningTotal[j] > maximum) { maximum = runningTotal[j + i] - runningTotal[j]; }
+    }
+    
+    cout << maximum << '\n';
+    
     return 0;
     
 }
