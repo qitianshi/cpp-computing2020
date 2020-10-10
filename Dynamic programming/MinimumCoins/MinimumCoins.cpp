c8baf1ab1d4b52a619ce04989c1d9d86c5e6ba19
qitianshi <tianshi.qi@icloud.com>
2020-10-10 23:55:10 +0800

    Adds documentation to MinimumCoins

--- Dynamic programming/MinimumCoins/MinimumCoins.cpp
+++ Dynamic programming/MinimumCoins/MinimumCoins.cpp
@@ -12,16 +12,20 @@
 #include <limits.h>     // INT_MAX
 using namespace std;
 
+/// Finds the minimum number of coins that can add to a given total.
+/// @param denominations An array of the possible denominations of coins.
+/// @param numberOfDenominations The number of denominations.
+/// @param remainingAmount The remaining unallocated amount. Initial value should be equal the total amount.
 int findMinimum(int denominations[], int numberOfDenominations, int remainingAmount) {
     
-    if (remainingAmount == 0) { return 0; }
+    if (remainingAmount == 0) { return 0; }     // Function returns if everything has been allocated.
     
-    int minimumNumber = INT_MAX;
+    int minimumNumber = INT_MAX;        // Initializes minimumNumber with INT_MAX so it will take the value of the first result.
     
-    for (int i = 0; i < numberOfDenominations; ++ i) {
-        if (denominations[i] <= remainingAmount) {
-            int solution = findMinimum(denominations, numberOfDenominations, remainingAmount - denominations[i]);
-            if (solution != INT_MAX && solution + 1 < minimumNumber) { minimumNumber = solution + 1; }
+    for (int i = numberOfDenominations - 1; i >= 0; -- i) {     // Tries the largest denominations first.
+        if (denominations[i] <= remainingAmount) {      // Any denomination larger than the remainder is ignored.
+            int solution = findMinimum(denominations, numberOfDenominations, remainingAmount - denominations[i]);       // Recursively breaks down the remainder until nothing remains.
+            minimumNumber = min(minimumNumber, solution + 1);       // Updates its value if the new solution is better.
         }
     }
     
